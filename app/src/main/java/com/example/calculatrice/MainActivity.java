66cac2823ec7292c65dfc86a4c3b4ff20319740e
dimit <dimitriaigle@gmail.com>
2022-07-11 09:22:08 -0400

    Fin calculatrice

--- app/src/main/java/com/example/calculatrice/MainActivity.java
+++ app/src/main/java/com/example/calculatrice/MainActivity.java
@@ -1,21 +1,23 @@
 package com.example.calculatrice;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.text.SpannableStringBuilder;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.mariuszgromada.math.mxparser.Expression;
+
 import java.util.ArrayList;
-import org.mariuszgromada.math.mxparser.*;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0;
-    private Button btnClear,btnEgal,btnPourcentage,btnDiviser,btnMultiply,
-    btnAdd,btnSou,btnPO,btnPF,btnPoint;
+    private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0;
+    private Button btnClear, btnEgal, btnPourcentage, btnDiviser, btnMultiply,
+            btnAdd, btnSou, btnPO, btnPF, btnPoint, btnCos, btnSin, btnTan, btnCos1, btnSin1,
+            btnTan1, btnLog, btnLn, btnRacine, btnE, btnPi, btnModulo, btnCarré, btnY, Btn1surX;
     private TextView calcul;
     private ImageButton sup;
     private TextView resultat;
@@ -29,34 +31,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Association des Buttons Numéros
-        btn0 =(Button) findViewById(R.id.but0);
-        btn1 =(Button) findViewById(R.id.but1);
-        btn2 =(Button) findViewById(R.id.but2);
-        btn3 =(Button) findViewById(R.id.but3);
-        btn4 =(Button) findViewById(R.id.but4);
-        btn5 =(Button) findViewById(R.id.but5);
-        btn6 =(Button) findViewById(R.id.but6);
-        btn7 =(Button) findViewById(R.id.but7);
-        btn8 =(Button) findViewById(R.id.but8);
-        btn9 =(Button) findViewById(R.id.but9);
+        btn0 = (Button) findViewById(R.id.but0);
+        btn1 = (Button) findViewById(R.id.but1);
+        btn2 = (Button) findViewById(R.id.but2);
+        btn3 = (Button) findViewById(R.id.but3);
+        btn4 = (Button) findViewById(R.id.but4);
+        btn5 = (Button) findViewById(R.id.but5);
+        btn6 = (Button) findViewById(R.id.but6);
+        btn7 = (Button) findViewById(R.id.but7);
+        btn8 = (Button) findViewById(R.id.but8);
+        btn9 = (Button) findViewById(R.id.but9);
 
         //Association des Buttons Opérateur
-        btnClear =(Button) findViewById(R.id.butClear);
-        btnPoint =(Button) findViewById(R.id.butPoint);
-        btnEgal =(Button) findViewById(R.id.butEgal);
-        btnPourcentage =(Button) findViewById(R.id.butPourcentage);
-        btnDiviser =(Button) findViewById(R.id.butDivision);
-        btnMultiply =(Button) findViewById(R.id.butmultiplication);
-        btnSou =(Button) findViewById(R.id.butMoins);
-        btnAdd =(Button) findViewById(R.id.butPlus);
+        btnClear = (Button) findViewById(R.id.butClear);
+        btnPoint = (Button) findViewById(R.id.butPoint);
+        btnEgal = (Button) findViewById(R.id.butEgal);
+        btnPourcentage = (Button) findViewById(R.id.butPourcentage);
+        btnDiviser = (Button) findViewById(R.id.butDivision);
+        btnMultiply = (Button) findViewById(R.id.butmultiplication);
+        btnSou = (Button) findViewById(R.id.butMoins);
+        btnAdd = (Button) findViewById(R.id.butPlus);
         btnPO = (Button) findViewById(R.id.butPO);
         btnPF = (Button) findViewById(R.id.butPF);
+        btnCos = (Button) findViewById(R.id.butCos);
+        btnSin = (Button) findViewById(R.id.butSin);
+        btnTan = (Button) findViewById(R.id.butTan);
+        btnCos1 = (Button) findViewById(R.id.butCos2);
+        btnSin1 = (Button) findViewById(R.id.butSin2);
+        btnTan1 = (Button) findViewById(R.id.butTan2);
+        btnLog = (Button) findViewById(R.id.butLog);
+        btnLn = (Button) findViewById(R.id.butLn);
+        btnRacine = (Button) findViewById(R.id.butRacine);
+        btnE = (Button) findViewById(R.id.butE);
+        btnPi = (Button) findViewById(R.id.butPi);
+        btnModulo = (Button) findViewById(R.id.butModulo);
+        btnCarré = (Button) findViewById(R.id.xCarré);
+        btnY = (Button) findViewById(R.id.xpowY);
+        Btn1surX = (Button) findViewById(R.id.unSurx);
+
 
         //Association des Champs Texte
-        calcul= (TextView)findViewById(R.id.textCalcul);
-        resultat= (TextView) findViewById(R.id.textResultat);
+        calcul = (TextView) findViewById(R.id.textCalcul);
+        resultat = (TextView) findViewById(R.id.textResultat);
 
-        sup=(ImageButton) findViewById(R.id.sup);
+        sup = (ImageButton) findViewById(R.id.sup);
 
 
         btn1.setOnClickListener(new View.OnClickListener() {
@@ -167,6 +185,96 @@ public class MainActivity extends AppCompatActivity {
                 CalculExpression("+");
             }
         });
+        btnCos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("cos(");
+            }
+        });
+        btnSin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("sin(");
+            }
+        });
+        btnTan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("tan(");
+            }
+        });
+        btnCos1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("arccos(");
+            }
+        });
+        btnSin1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("arcsin(");
+            }
+        });
+        btnTan1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("arctan(");
+            }
+        });
+        btnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("log(");
+            }
+        });
+        btnLn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("ln(");
+            }
+        });
+        btnRacine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("sqrt(");
+            }
+        });
+        btnE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("e");
+            }
+        });
+        btnPi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("pi");
+            }
+        });
+        btnModulo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("abs(");
+            }
+        });
+        btnCarré.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("^(2)");
+            }
+        });
+        btnY.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("^(");
+            }
+        });
+        Btn1surX.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculExpression("1/");
+            }
+        });
         btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -180,8 +288,8 @@ public class MainActivity extends AppCompatActivity {
 
                 Expression ligne = new Expression(expression);
                 String result = String.valueOf(ligne.calculate());
-                    resultat.setText(result);
-                }
+                resultat.setText(result);
+            }
         });
 
         resultat.setOnClickListener(new View.OnClickListener() {
@@ -195,22 +303,22 @@ public class MainActivity extends AppCompatActivity {
         sup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String expression = calcul.getText().toString();
-                calcul.setText(expression-1);
+                String s = calcul.getText().toString();
+                s =s.substring(0,s.length()-1);
+                calcul.setText(s);
             }
         });
 
 
-
     }
 
-    void CalculExpression(String value){
+    void CalculExpression(String value) {
         String expression = calcul.getText().toString();
         expression = expression + value;
         calcul.setText(expression);
     }
 
-    void Clear(){
+    void Clear() {
         calcul.setText("");
         resultat.setText("0");
     }
